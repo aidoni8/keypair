f5c44d6da8fc28c12663774c9746b9267e8d140e
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-01-18 19:20:38 -0600

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "aidoni" {
+resource "aws_key_pair" "github" {
   key_name   = "aidoni-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/wDS3B/P0QjYhpLTyUt44Yg/1sT94ZFgCDjTrI1gMk7LpXRjxToh9Tln4v4zqxBbrRkiVdFfZQ2T4gNGvkCRcL37jou9f7S9c41Fmp4poDsMB4zPE6Z4+56zvHJZitY521i+BtRvnnq2A7OIft1zYggQUxGRGI+8dkiLN/61DDFbXPl9E7OqAxvx+ViqBhqAH+7YFxukumMs0mAL3E6MzjQM9bOkjgavlgz5GNj4i7tnPnO+lLhjzm71fx00tpkJMC3T+TMP3PMLgg247aXTeTJcskJONw9J0E+Z/BqaS1exi2xP5CccLFfCnDmrUkoJsRDmRdTMazB0ZkyQqfEk1M7w7C5eMOXlku5rcr5l2TRC62jZeidfLBvQGKN0k3i4xTuEG4teaZJ3RRrbIFU8Z2Y0ZHAw+8muRygOHe5WkNeo333BnFYuyDX1W8XQI3aNPPMTX7EpOmuaUy0aPgYQ0gHL8sGILaX03tkVpkbpDZwCP3wgq8r2U8z45DbKS70c= dzhokish@Zhanarbeks-MacBook-Pro.local"
 }
