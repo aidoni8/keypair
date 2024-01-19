8c407d9d233f2198d26c62f8d4b856a4ea38aedc
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-01-18 19:37:17 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws-ec2" {
-  region  = "us-east-2"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
